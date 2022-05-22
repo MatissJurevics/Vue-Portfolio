03b8b825366f883f5cc0531bedc35f5f3a17b096
matiss <matissjurevics@gmail.com>
2022-05-22 20:55:01 +0100

    started pagee 3

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -2,27 +2,34 @@
 
 <template>
   <section
-    class="h-screen w-screen p-20 snap-center bg-[#000b2e] relative flex flex-col justify-center items-center sm:items-start overflow-hidden"
+    class="h-screen w-screen p-20 snap-center bg-[#000b2e] relative flex flex-col items-center sm:items-start overflow-hidden"
   >
-    <div class="z-10">
-        <h1 class="text-5xl lg:text-7xl font-bold text-white">About Me...</h1>
-    <p class="font-mono mt-4 text-white/70 w-[300px] lg:w-[600px] text-center sm:text-left">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, accusantium
-      quae. Perspiciatis animi cumque voluptates aperiam ab qui veniam quidem
-      quaerat impedit, similique, modi distinctio. Pariatur blanditiis molestiae
-      corporis commodi!
-    </p>
-    <button
-      class="mt-8 p-2 w-32 bg-transparent border-2 border-pink-500 rounded-xl text-pink-500 hover:bg-pink-500 hover:text-white hover:shadow-lg hover:shadow-pink-500/30 transition duration-300"
-    >
-      See My Work
-    </button></div> 
-        <li
-      class="animate-one animate-in absolute w-[500px] h-[500px]  lg:w-[700px] lg:h-[700px] rounded-full -top-32 -right-32 bg-fuchsia-400/50 blur-3xl"
+    <div class="z-10 w-full">
+      <h1 class="text-5xl lg:text-7xl font-bold text-white">My work</h1>
+      <p
+        class="font-mono mt-4 text-white/70 w-[300px] lg:w-[800px] text-center sm:text-left"
+      >
+        Here are a few examples of different kinds of work that I have done over
+        the years varying from UI design products to decentralized web
+        applications. These projects show my wide area of expertise while
+        hopefully showing that I have a strong understanding of the fundamentals.
+      </p>
+      <div class="w-full h-[550px] mt-12 bg-white/5 ">
+        <h2>Search By topic:</h2>
+        <ul>
+          <li>Web Develoopment</li>
+          <li>UI design</li>
+        </ul>
+        <div>
+
+        </div>
+      </div>
+    </div>
+    <li
+      class="animate-one animate-in absolute w-[500px] h-[500px] lg:w-[700px] lg:h-[700px] rounded-full -top-32 -right-32 bg-fuchsia-400/50 blur-3xl"
     ></li>
     <li
-      class="animate-two animate-in absolute w-[500px] h-[500px]  lg:w-[700px] lg:h-[700px] rounded-full -top-48 -left-48 bg-teal-400/50 blur-3xl"
+      class="animate-two animate-in absolute w-[500px] h-[500px] lg:w-[700px] lg:h-[700px] rounded-full -top-48 -left-48 bg-teal-400/50 blur-3xl"
     ></li>
   </section>
-
 </template>
