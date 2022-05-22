3aeb5523232dbcd16f864341636593124a819dc5
matiss <matissjurevics@gmail.com>
2022-05-22 13:46:45 +0100

    initialised project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Main from "./components/Main.vue"
+import About from "./components/About.vue"
+import Work from "./components/Work.vue"
+import Contact from "./components/Contact.vue"
+
+
+</script>
+
+<template>
+  <Main />
+  <About />
+  <Work />
+  <Contact />
+
+</template>
